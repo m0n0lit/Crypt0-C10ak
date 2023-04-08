a31eb99c89783dc3e6b9244ebf1d6356f8d3d94f
m0n0lit <47145298+m0n0lit@users.noreply.github.com>
2023-04-09 02:07:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,106 @@
-import os
-import random
-import string
-import logging
+import tkinter as tk
+from tkinter import messagebox
 import requests
-
-from bitcoin import *
+import json
+import logging
+import os
+import stem
+import stem.process
+from stem.util import term
 
 # Logging setup
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Tor network setup
-proxies = {
-    'http': 'socks5h://localhost:9050',
-    'https': 'socks5h://localhost:9050'
-}
+# GUI setup
+root = tk.Tk()
+root.title("Crypt0-C10ak")
 
-# Generate a random string of length n
-def random_string(n):
-    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(n))
+# Tor setup
+SOCKS_PORT = 7000
 
-# Generate a 2048-bit Bitcoin wallet
-def generate_wallet():
-    # Generate a random string of length 32
-    random_str = random_string(32)
-    # Generate a random private key
-    priv_key = sha256(random_str)
-    # Generate a public key
-    pub_key = privtopub(priv_key)
-    # Generate a Bitcoin address
-    address = pubtoaddr(pub_key)
-    # Return the address and private key
-    return address, priv_key
+def start_tor():
+    """
+    Starts a Tor process and configures the SOCKS port
+    """
+    try:
+        logging.info("Starting Tor process")
+        tor_process = stem.process.launch_tor_with_config(
+            config={
+                'SocksPort': str(SOCKS_PORT)
+            },
+            init_msg_handler=print_bootstrap_lines
+        )
+        logging.info("Tor process started")
+    except Exception as e:
+        logging.error("Unable to start Tor process: %s" % e)
 
-# Bitcoin mixer
-def mix_coins(address, priv_key):
-    # Send a request to the Bitcoin mixer
-    response = requests.post('https://mixer.example.com',
-                             data={'address': address, 'priv_key': priv_key},
-                             proxies=proxies)
-    # Log the response
-    logging.info('Mixer response: %s', response.text)
-    # Return the response
-    return response
+def print_bootstrap_lines(line):
+    """
+    Prints bootstrap lines
+    """
+    if "Bootstrapped " in line:
+        logging.info(term.format(line, term.Color.BLUE))
 
-# Main function
-def main():
-    # Generate a 2048-bit Bitcoin wallet
-    address, priv_key = generate_wallet()
-    # Log the address and private key
-    logging.info('Generated address: %s', address)
-    logging.info('Generated private key: %s', priv_key)
-    # Mix the coins
-    response = mix_coins(address, priv_key)
+# Mixer setup
+def mix_coins(amount):
+    """
+    Mixes coins using a Tor connection
+    """
+    try:
+        # Start Tor process
+        start_tor()
 
-if __name__ == '__main__':
-    main()
+        # Set up Tor session
+        session = requests.session()
+        session.proxies = {
+            'http': 'socks5://127.0.0.1:%s' % SOCKS_PORT,
+            'https': 'socks5://127.0.0.1:%s' % SOCKS_PORT
+        }
+
+        # Make request to mixer API
+        url = "https://example.com/mixer/mix"
+        data = {
+            "amount": amount
+        }
+        response = session.post(url, data=data)
+        response_data = json.loads(response.text)
+
+        # Stop Tor process
+        tor_process.kill()
+
+        # Return mixed coins
+        return response_data["mixed_coins"]
+    except Exception as e:
+        logging.error("Unable to mix coins: %s" % e)
+        messagebox.showerror("Error", "Unable to mix coins")
+
+# GUI setup
+def mix_coins_gui():
+    """
+    Mixes coins using a Tor connection
+    """
+    try:
+        # Get amount from GUI
+        amount = amount_entry.get()
+
+        # Mix coins
+        mixed_coins = mix_coins(amount)
+
+        # Show success message
+        messagebox.showinfo("Success", "Mixed coins: %s" % mixed_coins)
+    except Exception as e:
+        logging.error("Unable to mix coins: %s" % e)
+
+# GUI elements
+amount_label = tk.Label(root, text="Amount")
+amount_label.grid(row=0, column=0)
+
+amount_entry = tk.Entry(root)
+amount_entry.grid(row=0, column=1)
+
+mix_button = tk.Button(root, text="Mix Coins", command=mix_coins_gui)
+mix_button.grid(row=1, column=0, columnspan=2)
+
+# Start GUI
+root.mainloop()
